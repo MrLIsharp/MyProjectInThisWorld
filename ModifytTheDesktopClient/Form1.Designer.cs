204deda7a972ab01426d4cc8476f6ea0509b6bc5
deleteThis <673905504@qq.com>
2019-05-28 23:43:49 +0800

    试试

---
+++ ModifytTheDesktopClient/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ModifytTheDesktopClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UploadImage = new System.Windows.Forms.Button();
+            this.RandomMTD = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // UploadImage
+            // 
+            this.UploadImage.Location = new System.Drawing.Point(13, 13);
+            this.UploadImage.Name = "UploadImage";
+            this.UploadImage.Size = new System.Drawing.Size(84, 29);
+            this.UploadImage.TabIndex = 0;
+            this.UploadImage.Text = "上传图片";
+            this.UploadImage.UseVisualStyleBackColor = true;
+            this.UploadImage.Click += new System.EventHandler(this.UploadImage_Click);
+            // 
+            // RandomMTD
+            // 
+            this.RandomMTD.Location = new System.Drawing.Point(137, 13);
+            this.RandomMTD.Name = "RandomMTD";
+            this.RandomMTD.Size = new System.Drawing.Size(84, 29);
+            this.RandomMTD.TabIndex = 1;
+            this.RandomMTD.Text = "随机桌面";
+            this.RandomMTD.UseVisualStyleBackColor = true;
+            this.RandomMTD.Click += new System.EventHandler(this.RandomMTD_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 52);
+            this.Controls.Add(this.RandomMTD);
+            this.Controls.Add(this.UploadImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.Text = "隨機換壁紙";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UploadImage;
+        private System.Windows.Forms.Button RandomMTD;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ModifytTheDesktopClient/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ModifytTheDesktopClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+        /// <summary>
+        /// 上傳文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UploadImage_Click(object sender, EventArgs e)
+        {
+            //初始化openFileDialog
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择要上传的图片";
+            ofd.Filter = "图像文件(*.jpg;*.gif;*.png)|*.jpg;*.gif;*.png";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string oldFilePath = ofd.FileName;
+                int position = oldFilePath.LastIndexOf("\\");
+                string OldfileName = oldFilePath.Substring(position + 1);
+                string[] splitName = oldFilePath.Split('.');
+                string ext = splitName[splitName.Length - 1];
+
+                //string newName = DateTime.Now.ToString("yyyyMMddhhmmss") + ".bmp";// + ext;
+                var index = 1;
+            cc: string newName = index + ".bmp";// + ext;
+
+                //判断根目录下是否含有指定文件夹，若没有则创建一个新的
+                string path = AppDomain.CurrentDomain.BaseDirectory + "/UpLoadImages";
+                DirectoryInfo di = new DirectoryInfo(path);
+                if (!di.Exists)
+                {
+                    di.Create();
+                }
+                string newFilePath = AppDomain.CurrentDomain.BaseDirectory + "/UpLoadImages/" + newName;
+                if (File.Exists(newFilePath))
+                {
+                    index = index + 1;
+                    goto cc;
+                }
+                File.Copy(oldFilePath, newFilePath, false);
+                Image img = Image.FromFile(newFilePath);
+                //tureBox1.Image = img;
+                MessageBox.Show("上传成功", "Tip");
+            }
+        }
+        /// <summary>
+        /// 設置隨機壁紙
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RandomMTD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random random = new Random();
+                var FileName = AppDomain.CurrentDomain.BaseDirectory + "/UpLoadImages";
+                System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(FileName);
+                UpdateWindowsDesk(random.Next(1,GetFilesCount(dirInfo)+1));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        /// <summary>
+        /// 讀取文件夾下文件數量
+        /// </summary>
+        /// <param name="dirInfo"></param>
+        /// <returns></returns>
+        public static int GetFilesCount(DirectoryInfo dirInfo)
+        {
+
+            int totalFile = 0;
+            //totalFile += dirInfo.GetFiles().Length;//获取全部文件
+            totalFile += dirInfo.GetFiles("*.bmp").Length;//获取某种格式
+            foreach (System.IO.DirectoryInfo subdir in dirInfo.GetDirectories())
+            {
+                totalFile += GetFilesCount(subdir);
+            }
+            return totalFile;
+        }
+        //引用Userdll
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(
+          int uAction,
+          int uParam,
+          string lpvParam,
+          int fuWinIni
+          );
+        /// <summary>
+        /// 設置圖片桌面
+        /// </summary>
+        /// <param name="randomFileName"></param>
+        public static void UpdateWindowsDesk(int randomFileName)
+        {
+            //Image image = Image.FromFile("E:\\新建文件夹\\test.jpg");
+            //var PicName = "E:\\新建文件夹\\" + DateTime.Now.ToShortTimeString() + ".bmp";
+            var PicName = AppDomain.CurrentDomain.BaseDirectory + @"UpLoadImages\"+ randomFileName+".bmp";
+            //image.Save(PicName, System.Drawing.Imaging.ImageFormat.Bmp);
+            SystemParametersInfo(20, 0, PicName, 0x2);
+           // MessageBox.Show(PicName);
+        }
+        /// <summary>
+        /// 設置圖片桌面
+        /// </summary>
+        /// <param name="randomFileName"></param>
+        /// <param name="towFileName"></param>
+        public static void UpdateWindowsDesk(int randomFileName,int towFileName)
+        {
+            var PicName = AppDomain.CurrentDomain.BaseDirectory + @"UpLoadImages\" + randomFileName + ".bmp";
+            var PicName2 = AppDomain.CurrentDomain.BaseDirectory + @"UpLoadImages\" + towFileName + ".bmp";
+            SystemParametersInfo(20, 0, PicName, 0x2);
+            SystemParametersInfo(20, 0, PicName2, 0x2);
+        }
+        /// <summary>
+        /// 推出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {               
+                this.Close();   //关闭程序
+            }
+        }      
+    }
+}
